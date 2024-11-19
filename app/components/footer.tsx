8316b22e127510cdcfb7f8ca459ce8cede5d57f2
Henrique Horbovyi <henrikhorbovyi@gmail.com>
2024-11-19 16:36:25 +0000

    removed view source and adding github link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -18,26 +18,13 @@ function ArrowIcon() {
 export default function Footer() {
   return (
     <footer className="mb-16">
-      /**
        <ul className="font-sm mt-8 flex flex-col space-x-0 space-y-2 text-neutral-600 md:flex-row md:space-x-4 md:space-y-0 dark:text-neutral-300">
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="/rss"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">rss</p>
-          </a>
-        </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://github.com/vercel/next.js"
-          >
+            href="github.com/henriquehorbovyi">
             <ArrowIcon />
             <p className="ml-2 h-7">github</p>
           </a>
@@ -47,14 +34,13 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
+            href="/rss"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
+            <p className="ml-2 h-7">github</p>
           </a>
         </li>
       </ul>
-      */
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
         © {new Date().getFullYear()} MIT Licensed
       </p>
