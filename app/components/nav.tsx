7f887d58fdea07aaf04ce1bba96915d314cb89be
Henrique Horbovyi <henrikhorbovyi@gmail.com>
2024-11-19 22:46:49 +0000

    adding theme switch

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { useTheme } from 'next-themes'
+import ThemeSwitch from './ThemeSwitch'
 
 const navItems = {
   '/': {
@@ -9,40 +9,28 @@ const navItems = {
     name: 'blog',
   },
 }
-
 export function Navbar() {
-  const { theme, setTheme } = useTheme()
-
   return (
     <aside className="-ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
+          className="flex flex-row justify-between items-center relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
           id="nav"
         >
-          <div className="flex flex-row space-x-0 pr-10">
-            {Object.entries(navItems).map(([path, { name }]) => {
-              return (
-                <Link
-                  key={path}
-                  href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1">
-                  {name}
-                </Link>
-              )
-            })}
+          <div className="flex flex-row space-x-0 pr-10"> 
+            {Object.entries(navItems).map(([path, { name }]) => (
+              <Link
+                key={path}
+                href={path}
+                className="transition-all hover:text-zinc-900 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1">
+                {name}
+              </Link>
+            ))}
           </div>
-
-          <button
-            className="p-2"
-            onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
-            aria-label="Toggle Dark Mode"
-          >
-            Toggle Dark Mode
-          </button>
-
+ 
+          <ThemeSwitch />
         </nav>
       </div>
     </aside>
-  )
+  );
 }
